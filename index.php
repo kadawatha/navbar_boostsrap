a7762b7acbacc57af764c9cf162ac389cf6e8f87
Dhanushka <skydigitalweb01@gmail.com>
2018-04-20 17:26:07 +0530

    Initial commit

---
+++ index.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8" />
+<meta http-equiv="x-ua-compatible" content="ie=edge, chrome=1" />
+<title>Responsive Navigation Bar</title>
+<link rel="stylesheet" type="text/css" href="css/style.css" />
+</head>
+<body>
+
+
+<div id="header">
+<div id="logo">
+    <img src="logo/E-Partner.png" alt="logo">
+</div>
+    <ul id="navbar">
+        <li>
+            <a href="#">Home</a>
+        </li>
+        <li>
+            <a href="#">Service</a>
+        </li>
+        <li>
+            <a href="#">Products</a>
+        </li>
+        <li>
+            <a href="#">About</a>
+        </li>
+        <li>
+            <a href="#">Contact</a>
+        </li>
+
+
+
+    </ul>
+
+
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<script src=""></script>
+</body>
+</html>
